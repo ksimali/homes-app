c85663a842f969b7411f4b0b3cb6c5bab8151dab
ksimali <cassim.ali@outlook.fr>
2024-08-19 14:32:18 -0400

    Delete HousingLocationComponent in AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { HomeComponent } from './home/home.component';
-import { HousingLocationComponent } from './housing-location/housing-location.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HomeComponent,HousingLocationComponent],
+  imports: [HomeComponent],
   template: `
   <main>
     <header class="brand-name">
