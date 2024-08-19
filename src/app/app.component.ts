9949e830f13462b820b53a95ccaf94a6c12445b9
ksimali <cassim.ali@outlook.fr>
2024-08-19 13:01:17 -0400

    Delete app.component.html +  update app.component.ts and index.html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  imports: [],
+  template: `<h1>Hello World!</h1>`,
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'homes-app';
+  title = 'homes';
 }
