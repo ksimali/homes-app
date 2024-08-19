31ed45c0f3e3bfa34093504a6d9f54e878fc4998
ksimali <cassim.ali@outlook.fr>
2024-08-19 13:07:55 -0400

    update styleUrls in app.components.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   template: `<h1>Hello World!</h1>`,
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'homes';
